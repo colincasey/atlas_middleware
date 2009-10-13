862aab1121c7deeb9ea5445ae12119f462aef871
Colin Casey <casey.colin@gmail.com>
2009-10-13 15:25:57 -0300

    Legend info fetches legend urls instead of mime data

--- lib/map_server_legend_info.rb
+++ lib/map_server_legend_info.rb
@@ -7,7 +7,7 @@ class MapServerLegendInfo
     headers = { "Content-Type" => "application/json" }
 
     begin
-      response = Rack::Response.new(get_legends(request['url']), 200, headers)
+      response = Rack::Response.new(get_legends(request), 200, headers)
       response = maybe_cache_response(response)
     rescue Exception => e
       response = Rack::Response.new({ :error => e.message }.to_json, 500, headers)
@@ -17,10 +17,10 @@ class MapServerLegendInfo
   end
 
   private
-  def get_legends(map_server_url)
-    map_server = ArcServer::MapServer.new(map_server_url)
+  def get_legends(request)
+    map_server = ArcServer::MapServer.new(request['url'])
     map_name = map_server.get_default_map_name
-    legend = map_server.get_legend_info(:map_name => map_name)
+    legend = map_server.get_legend_info(:map_name => map_name, :image_return_url => true)
     legend.to_json
   end
 
